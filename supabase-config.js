b0f32444269457361a14f9e696004f50c89a8c7e
mariah.lightfoot <mariah.lightfoot@capacity.com>
2026-04-20 13:21:24 -0500

    add cloudflare proxy url

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,4 @@
 // Paste your values from Supabase → Project Settings → API
 const SUPABASE_URL = 'https://rdicceacwndrzkctbmzj.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkaWNjZWFjd25kcnprY3RibXpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2ODQ2NzgsImV4cCI6MjA5MjI2MDY3OH0._WNdeH_Necs0SpBikZaSBz2L0rc0d7_VSj-CKhZkgH4';
+const PROXY_URL = 'https://stfu-jira-proxy.mariah-lightfoot.workers.dev';
